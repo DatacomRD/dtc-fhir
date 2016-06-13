5727f230fbcc98e6476a73ef4ac38b7310010e90
MontyPan <monty.pan@dontcareabout.us>
2016-06-13 19:27:32 +0800

    補強 TypeVariable、WildcardType 的處理。
    精簡程式寫法。

--- src/main/java/com/dtc/common/reflection/TypeUtil.java
+++ src/main/java/com/dtc/common/reflection/TypeUtil.java
@@ -2,35 +2,53 @@ package com.dtc.common.reflection;
 
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
+import java.lang.reflect.WildcardType;
 
 public class TypeUtil {
+	public static String toDeclaration(Type type) {
+		return toDeclaration(type, false);
+	}
+
 	/**
 	 * 將 {@link Type} 轉換成 Java 中宣告變數的形式（包含泛型處理）
 	 */
-	public static String toDeclaration(Type type) {
-//		System.out.println(type.getClass().getName());
-		StringBuffer result = new StringBuffer();
+	public static String toDeclaration(Type type, boolean full) {
+//		System.out.println("==> " + type.getClass().getName() + " = " + type);
 
 		if (type instanceof ParameterizedType) {
-			result.append(toString((ParameterizedType) type));
-		} else {
-			result.append(classTypeToString(type));
-		}
+			return toString((ParameterizedType) type, full);
+		} else if (type instanceof TypeVariable) {
+			StringBuffer result = new StringBuffer();
+			TypeVariable<?> tt = (TypeVariable<?>) type;
+			result.append(tt.getName());
 
-		return result.toString();
+			if (full && tt.getBounds().length > 0) {
+				result.append(" extends ");
+				StringBuffer boundResult = new StringBuffer();
+
+				for (Type bound : tt.getBounds()) {
+					boundResult.append(toDeclaration(bound, full));
+					boundResult.append(",");
+				}
+
+				result.append(boundResult.substring(0, boundResult.length() - 1));
+			}
+
+			return result.toString();
+		} else if (type instanceof WildcardType) {
+			return "?";
+		} else {
+			return classTypeToString(type);
+		}
 	}
 
-	private static String toString(ParameterizedType type) {
+	private static String toString(ParameterizedType type, boolean full) {
 		StringBuffer result = new StringBuffer(toDeclaration(type.getRawType()));
 		result.append("<");
 
 		for (Type t : type.getActualTypeArguments()) {
-			if (t instanceof ParameterizedType) {
-				result.append(toString((ParameterizedType) t));
-			} else {
-				result.append(classTypeToString(t));
-			}
-
+			result.append(toDeclaration(t));
 			result.append(",");
 		}
 
