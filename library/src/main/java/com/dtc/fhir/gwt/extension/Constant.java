c45986548238477f5b374e042b09b496a72c03c7
MontyPan <monty.pan@dontcareabout.us>
2016-09-13 17:53:38 +0800

    修正：Instant.getValue() 始終為 null 的問題

---
+++ library/src/main/java/com/dtc/fhir/gwt/extension/Constant.java
@@ -0,0 +1,8 @@
+package com.dtc.fhir.gwt.extension;
+
+public class Constant {
+	/** FHIR 的時間字串格式 */
+	public static final String DATETIME_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX";
+
+	//XXX 是否要提供 GWT 的 DateTimeFormat？
+}
