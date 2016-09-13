46bbad2938b7d7442d95118a3715fb40492ec73f
Gaduo <foxb1249@gmail.com>
2016-09-13 17:49:43 +0800

    常數類別

---
+++ library/src/main/java/com/dtc/fhir/vo/Context.java
@@ -0,0 +1,12 @@
+package com.dtc.fhir.vo;
+
+public class Context {
+
+	public static final String PARAM_GETPAGES = "_getpages";
+
+	public static final String PARAM_GETPAGESOFFSET = "_getpagesoffset";
+
+	public static final String PARAM_COUNT = "_count";
+	
+	public static final int FHIR_COUNT_LIMIT = 50;
+}
