1ed600f370c2ffc5ea433bf7b0484f19f34f6064
Yu Wei Chien <foxb1249@gmail.com>
2016-09-20 15:26:34 +0800

    assert  findOne 為 not null 且 id 相同

--- library/src/test/java/com/dtc/test/fhir/gwt/CmmunicationGwtRepoTest.java
+++ library/src/test/java/com/dtc/test/fhir/gwt/CmmunicationGwtRepoTest.java
@@ -3,6 +3,7 @@ package com.dtc.test.fhir.gwt;
 import com.dtc.fhir.gwt.Communication;
 import com.dtc.fhir.gwt.ResourceContainer;
 import com.dtc.fhir.repository.gwt.BaseGwtRepo;
+import com.dtc.fhir.repository.hapi.SimpleHapiRepo;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -26,5 +27,27 @@ public class CmmunicationGwtRepoTest {
 	public void testCommunicationFindOneAssertNull() {
 		Communication resource = cmmunicationGwtRepo.findOne("1");
 		Assert.assertNull(resource);
+	}
+
+	private SimpleHapiRepo<ca.uhn.fhir.model.dstu2.resource.Communication> simpleHapiRepo = new SimpleHapiRepo<>(baseUrl, ca.uhn.fhir.model.dstu2.resource.Communication.class);
+
+	/**
+	 * 測試流程：
+	 * <ol>
+	 *     <li>先利用 simpleHapiRepo save 一個 id 為 test.id 的 communication resource 到 FHIR server</li>
+	 *     <li>再利用 cmmunicationRepo 查詢 id 為 "test.id" 的 resource</li>
+	 *     <li>預期是 not null 以及 id 為 "test.id"</li>
+	 * </ol>
+	 */
+	@Test
+	public void testCommunicationFindOneAssertNotNull() {
+		final String ID = "test.id";
+		ca.uhn.fhir.model.dstu2.resource.Communication communication = new ca.uhn.fhir.model.dstu2.resource.Communication();
+		communication.setId(ID);
+		simpleHapiRepo.save(communication);
+
+		Communication resource = cmmunicationGwtRepo.findOne(ID);
+		Assert.assertNotNull(resource);
+		Assert.assertEquals(ID, resource.getId().getValue());
 	}
 }
